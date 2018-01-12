7c48170a21db542a5c50a81d8a3a91d6d59f77f6
Sam.Shen <samfec@163.com>
2018-01-12 11:25:31 +0800

    。

--- src/7.RGB和Hex转换.js
+++ src/7.RGB和Hex转换.js
@@ -9,17 +9,11 @@
  rgb(148, 0, 211) // returns 9400D3
  */
 function rgb(r, g, b){
-    // complete this function
-    const converted = {10: 'A',11: 'B',12: 'C',13: 'D',14:'E',15:'F' };
-
-    function convert(rgbNumber){
-        if(rgbNumber> 255) rgbNumber = 255;
-        if(rgbNumber < 0 ) rgbNumber = 0;
-        const remainder = rgbNumber % 16;
-        const mainNum = (rgbNumber - remainder) / 16;
-
-        return `${converted[mainNum] || mainNum}${converted[remainder] || remainder}`
+    function toHex(d) {
+        if(d < 0 ) {return "00";}
+        if(d > 255 ) {return "FF";}
+        return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase();
     }
 
-    return `${convert(r)}${convert(g)}${convert(b)}`
+    return toHex(r)+toHex(g)+toHex(b);
 }
