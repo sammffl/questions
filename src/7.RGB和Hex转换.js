401d96578dec6e4dfde58df6bde2ca4e93abcb60
Sam.Shen <samfec@163.com>
2018-01-12 11:09:52 +0800

    、

---
+++ src/7.RGB和Hex转换.js
@@ -0,0 +1,10 @@
+/*
+实现一个rgb()方法 输入RGB数值返回HEX值
+有效数值在0~255之间
+
+预期结果
+ rgb(255, 255, 255) // returns FFFFFF
+ rgb(255, 255, 300) // returns FFFFFF
+ rgb(0,0,0) // returns 000000
+ rgb(148, 0, 211) // returns 9400D3
+ */
