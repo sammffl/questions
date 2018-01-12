a7fcca0e4c2a33c0e892243d09c68eedd9770e1d
Sam.Shen <samfec@163.com>
2018-01-12 11:12:34 +0800

    添加转换rgb到hex解题思路

--- src/7.RGB和Hex转换.js
+++ src/7.RGB和Hex转换.js
@@ -8,3 +8,18 @@
  rgb(0,0,0) // returns 000000
  rgb(148, 0, 211) // returns 9400D3
  */
+function rgb(r, g, b){
+    // complete this function
+    const converted = {10: 'A',11: 'B',12: 'C',13: 'D',14:'E',15:'F' };
+
+    function convert(rgbNumber){
+        if(rgbNumber> 255) rgbNumber = 255;
+        if(rgbNumber < 0 ) rgbNumber = 0;
+        const remainder = rgbNumber % 16;
+        const mainNum = (rgbNumber - remainder) / 16;
+
+        return `${converted[mainNum] || mainNum}${converted[remainder] || remainder}`
+    }
+
+    return `${convert(r)}${convert(g)}${convert(b)}`
+}
